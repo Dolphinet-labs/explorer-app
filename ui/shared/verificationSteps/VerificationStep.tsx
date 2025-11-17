f596f7d5cb4dbe7b8a529e14a7fbfc40e8cd2003
Aaron <x.g@u.nus.edu>
2025-11-17 16:43:41 +0800

    fix: 淇 verification steps 鍥炬爣璺緞

--- ui/shared/verificationSteps/VerificationStep.tsx
+++ ui/shared/verificationSteps/VerificationStep.tsx
@@ -23,7 +23,9 @@ const VerificationStep = ({ step, isLast, isPassed, isPending, noIcon }: Props) 
 
   return (
     <HStack gap={ 2 } color={ stepColor }>
-      { !noIcon && <IconSvg name={ isPassed ? 'verification-steps/finalized' : 'verification-steps/unfinalized' } boxSize={ 5 }/> }
+      { !noIcon && (
+        <IconSvg name={ isPassed ? 'verification-steps\\finalized' : 'verification-steps\\unfinalized' } boxSize={ 5 }/>
+      ) }
       <Box color={ stepColor }>{ typeof step === 'string' ? step : step.content }</Box>
       { !isLast && <IconSvg name={ 'arrows\\east' } boxSize={ 5 }/> }
     </HStack>
