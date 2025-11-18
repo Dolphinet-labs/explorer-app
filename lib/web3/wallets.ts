372ac90c4708395a2759c5d738519b3c696f570f
Aaron <x.g@u.nus.edu>
2025-11-18 10:17:10 +0800

    fix: replace backslashes with forward slashes in wallet icon paths

--- lib/web3/wallets.ts
+++ lib/web3/wallets.ts
@@ -3,14 +3,14 @@ import type { WalletType, WalletInfo } from 'types/client/wallets';
 export const WALLETS_INFO: Record<Exclude<WalletType, 'none'>, WalletInfo> = {
   metamask: {
     name: 'MetaMask',
-    icon: 'wallets\\metamask',
+    icon: 'wallets/metamask',
   },
   coinbase: {
     name: 'Coinbase Wallet',
-    icon: 'wallets\\coinbase',
+    icon: 'wallets/coinbase',
   },
   token_pocket: {
     name: 'TokenPocket',
-    icon: 'wallets\\token-pocket',
+    icon: 'wallets/token-pocket',
   },
 };
